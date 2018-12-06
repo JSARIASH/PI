0bac109c11abc4dc41ea50a6399c8fa067a7a32a
SebastianArias <sebasariash@gmail.com>
2018-12-06 10:19:52 -0500

    Espacios

--- Simulacion.R
+++ Simulacion.R
@@ -1,13 +1,12 @@
 ## Valor de Pi mediante simulación
 n <- 902395
-x <- runif(n,0,1)
-y <- runif(n,0,1)
-z <- x^2+y^2
+x <- runif(n, 0, 1)
+y <- runif(n, 0, 1)
+z <- x ^ 2 + y ^ 2
 r <- which(z > 1)
 r1 <- which(z <= 1)
 
-plot(x[r1],y[r1],pch=".",col="red",xlim = c(0,1),ylim = c(0,1),xlab = "",ylab ="" )
+plot(x[r1], y[r1], pch = ".", col = "red", xlim = c(0, 1), ylim = c(0, 1), xlab = "", ylab ="" )
 par(new = TRUE)
-plot(x[r],y[r],pch=".",col="black",xlim = c(0,1),ylim = c(0,1),xlab = "X",ylab = "Y")
-
-(length(r1)/n)*4
+plot(x[r], y[r], pch = ".", col = "black", xlim = c(0,1), ylim = c(0, 1), xlab = "X", ylab = "Y")
+(length(r1) / n) * 4
